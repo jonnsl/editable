197115679d93064ce056c1e4b4e37c9d8c45d164
Jonnathan Soares <jonnsl@hotmail.com>
2024-08-27 17:36:09 -0300

    setup gh-pages

--- dist/cjs/ActionButtons.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { MouseEvent } from 'react';
-export type ActionButtonsProps = {
-    locked?: boolean | undefined;
-    colSpan?: number | undefined;
-    onConfirm: (e: MouseEvent<HTMLButtonElement>) => void;
-    onCancel: (e: MouseEvent<HTMLButtonElement>) => void;
-    onDelete: (e: MouseEvent<HTMLButtonElement>) => void;
-};
-export default function ActionButtons(props: ActionButtonsProps): React.JSX.Element;
--- dist/esm/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function isObject(value: any): boolean;
-export declare function empty(value: any): boolean;
-export declare function cloneObject(obj: any): any;
-export declare function reorder<T>(list: T[], startIndex: number, endIndex: number): T[];
-export declare function noop(): void;
